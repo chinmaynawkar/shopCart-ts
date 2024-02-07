ad43a113b9267a13dfd2a0c4f8dcbb1411edaa44
chinmaynawkar <119050434+chinmaynawkar@users.noreply.github.com>
2024-02-07 11:14:02 +0530

    api-for-products-fetching

---
+++ src/data/api.ts
@@ -0,0 +1,12 @@
+// api.ts
+import axios from 'axios';
+
+export const fetchProducts = async () => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products', error);
+    throw new Error('Error fetching products. Please try again.');
+  }
+};
